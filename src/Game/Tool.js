529915c70920190aa3c6b12c28384d8eaec6684b
18heim <christopher.heim@mines-paristech.fr>
2020-12-13 17:38:57 +0100

    tool's catalogue created and Tool class reworked

--- src/Game/Tool.js
+++ src/Game/Tool.js
@@ -1,4 +1,4 @@
-import { Movement } from './Movement'
+import { DIRECTIONS, Movement } from './Movement'
 import { Room } from './Room'
 import { say } from '../Interface/Text'
 
@@ -9,33 +9,47 @@ export class Tool {
    * @param {string} tool.description description of the tool in the room where we find it
    * @param {string} tool.errorMessage if we are not in the right room, show message showing that object can't be used here
    * @param {string} tool.useMessage if we are in the right room, message display when we use the object
-   * @param {Room} tool.room in which we need to use the object
+   * @param {number} tool.roomId in which we need to use the object
    * @param {newDescription} tool.roomDescription new description of the room the tool is used in
-   * @param {Movement} tool.movement movement unlocked by the use of this tool
+   * @param {Direction} tool.direction direction unlocked by the use of this tool
    */
-  constructor({ name, description = name, errorMessage = '', useMessage = '', room, roomDescription, movement }) {
+  constructor({ name, description = name, errorMessage = '', useMessage = '', roomId, roomDescription, direction }) {
     this.name = name
     this.description = description
     this.errorMessage = errorMessage
     this.useMessage = useMessage
-    this.room = room
+    this.roomId = roomId
     this.roomDescription = roomDescription
-    this.movement = movement
+    this.direction = direction
   }
 
   /**
    * @param {Room} currentRoom - The room in which player is now
    */
   use(currentRoom) {
-    if (this.room === currentRoom) {
+    if (this.roomId === +currentRoom.name) {
       say(this.useMessage);
-      this.movement.isAllowed = true;
-      this.changeRoomDescription();
+      switch (this.direction) {
+        case DIRECTIONS.UP: 
+          currentRoom.moves[0].isAllowed = true
+          break
+        case DIRECTIONS.DOWN: 
+          currentRoom.moves[1].isAllowed = true;
+          break;
+        case DIRECTIONS.LEFT: 
+          currentRoom.moves[2].isAllowed = true;
+          break;
+        case DIRECTIONS.RIGHT : 
+          currentRoom.moves[3].isAllowed = true;
+          break;
+      }
+
+      this.changeRoomDescription(currentRoom);
     }
     else say(this.errorMessage);
   }
 
-  changeRoomDescription() {
-    this.room.description = this.roomDescription;
+  changeRoomDescription(room) {
+    room.description = this.roomDescription;
   }
 }
